a2965f02b2974d9bd37d0680467c55b8dc46162b
KaronM7098 <Karonminor2004@gmail.com>
2024-04-25 21:12:55 -0400

    adding main files

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
+      <Text>My name is..</Text>
+      <TextInput style={styles.input}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +19,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: 20,
+    height: 10 ,
+    borderColor: 'black',
+  },
+  Text:{
+    color: 'red',
+  }
 });
